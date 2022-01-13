f5fa04692beea254de0351f8ac3a70623523bc82
Joel Ermantraut <joelermantraut@gmail.com>
2022-01-13 06:45:18 +0100

    Almost none changes

--- SocketPlot-Test.py
+++ SocketPlot-Test.py
@@ -1,19 +1,19 @@
 import asyncore, socket, time
-import numpy as np 
+import numpy as np
 from collections import deque
 from SoftOscilloscope import SocketClientPlot
 from multiprocessing import Process
 
 data = np.linspace(-np.pi, np.pi, 50)
 
-class Server(asyncore.dispatcher):   
+class Server(asyncore.dispatcher):
     def __init__(self, address):
         asyncore.dispatcher.__init__(self)
         self.create_socket(socket.AF_INET,socket.SOCK_STREAM)
         self.bind(address)
         self.listen(5)
-        return 
-        
+        return
+
     def handle_accept(self):
         global data
         client, addr = self.accept()
@@ -28,14 +28,14 @@ class Server(asyncore.dispatcher):
             except Exception as e:
                 print(e)
                 return
-        
+
     def handle_close(self):
         self.close()
         print("Closing server.")
 
 
 if __name__ == '__main__':
-    address = ('0.0.0.0',5000)
+    address = ('0.0.0.0', 5000)
     server = Server(address)
     try:
         print("Server listening on " + str(address))
@@ -43,4 +43,3 @@ if __name__ == '__main__':
     except KeyboardInterrupt:
         print("Closing server.")
         server.close()
-        
