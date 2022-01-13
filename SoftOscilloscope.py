9d50c8ed493cda6a037edf6feab624067cca172f
Joel Ermantraut <joelermantraut@gmail.com>
2022-01-13 06:46:03 +0100

    Moved to Python3, corrected mistakes

--- SoftOscilloscope.py
+++ SoftOscilloscope.py
@@ -2,11 +2,16 @@ import serial, socket, sys
 from pyqtgraph.Qt import QtGui, QtCore
 import pyqtgraph as pg
 import numpy as np
-import signal 
+import signal
+
+class ButtonPanel(object):
+    def __init__(self, **kwargs):
+        button = QtGui.QPushButton('button')
 
 class BasePlot(object):
     def __init__(self, stream, **kwargs):
         self.stream = stream
+        self.kwargs = kwargs
         try:
             self.app = QtGui.QApplication([])
         except RuntimeError:
@@ -18,13 +23,19 @@ class BasePlot(object):
         self.view.setCentralItem(self.layout)
         self.view.show()
         self.view.setWindowTitle('Software Oscilloscope')
-        self.view.resize(800,600)
+        self.view.showMaximized()
         self.plot_list = []
+
+    def set_options(self, plot, kwargs):
+        if "xlim" in kwargs.keys():
+            plot.setXRange(kwargs["xlim"][0], kwargs["xlim"][1])
+        elif "ylim" in kwargs.keys():
+            plot.setYRange(kwargs["ylim"][0], kwargs["ylim"][1])
 
     def open_stream(self):
         print("Opening Stream")
         self.stream.open()
-        
+
     def close_stream(self):
         if hasattr(self.stream, 'flushInput'):
             self.stream.flushInput()
@@ -32,41 +43,57 @@ class BasePlot(object):
             self.stream.flushOutput()
         self.stream.close()
         print("Stream closed")
-        
+
     def handle_close_event(self, event):
         self.close_stream()
         self.app.exit()
 
+    def read_stream(self):
+        try:
+            stream_data = self.stream.readline().rstrip().split(',')
+        except TypeError:
+            stream_data = self.stream.readline().decode('utf-8').rstrip().split(',')
+
+        return stream_data
+
     def plot_init(self):
-        for i in xrange(20):
-            trial_data = self.stream.readline().rstrip().split(',')
-        for i in xrange(len(trial_data)):
+        for i in range(20):
+            trial_data = self.read_stream()
+
+        for i in range(len(trial_data)):
             new_plot = self.layout.addPlot()
             new_plot.plot(np.zeros(250))
+            self.set_options(new_plot, self.kwargs)
             self.plot_list.append(new_plot.listDataItems()[0])
             self.layout.nextRow()
-        
+
     def update(self):
-        stream_data = self.stream.readline().rstrip().split(',')
+        stream_data = self.read_stream()
+
         for data, line in zip(stream_data, self.plot_list):
+            yData = line.yData
+            yData[-1] = data
+
             line.informViewBoundsChanged()
-            line.xData = np.arange(len(line.yData))
-            line.yData = np.roll(line.yData, -1)
-            line.yData[-1] = data
-            line.xClean = line.yClean = None
-            line.xDisp = None
-            line.yDisp = None
+            line.setData(
+                x=np.arange(len(line.yData)),
+                y=np.roll(yData, -1),
+                xClean=None,
+                yClean=None,
+                xDisp=None,
+                yDisp=None
+            )
             line.updateItems()
             line.sigPlotChanged.emit(line)
- 
+
     def start(self):
         self.open_stream()
         self.plot_init()
         timer = QtCore.QTimer()
         timer.timeout.connect(self.update)
-        timer.start(0)   
+        timer.start(0)
         if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
-            self.app.exec_()   
+            self.app.exec_()
 
 class SerialPlot(BasePlot):
     def __init__(self, com_port, baud_rate, **kwargs):
@@ -80,12 +107,12 @@ class SocketClientPlot(BasePlot):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket_params = (address, port)
         self.socket.connect((address, port))
-        self.stream = self.socket.makefile()  
+        self.stream = self.socket.makefile()
         super(SocketClientPlot, self).__init__(self.stream, **kwargs)
-        
+
     def open_stream(self):
         pass
-        
+
     def close_stream(self):
         self.socket.close()
         self.stream.close()
@@ -97,4 +124,4 @@ class GenericPlot(BasePlot):
         and hasattr(stream, 'readline'):
             super(GenericPlot, self).__init__(stream, **kwargs)
         else:
-            raise BadAttributeError("One of the open/close/readline attributes is missing")  
+            raise BadAttributeError("One of the open/close/readline attributes is missing")
