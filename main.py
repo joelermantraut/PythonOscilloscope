c316f7a3152312ece8c346fb11e562ce14446d7f
Joel Ermantraut <joelermantraut@gmail.com>
2023-02-06 23:44:48 -0300

    Added function to automatically select serial port (using CH340 description)

--- main.py
+++ main.py
@@ -7,15 +7,10 @@ import sys
 import serial.tools.list_ports
 
 def get_device_port():
-    ports = list(serial.tools.list_ports.comports())
-    for p in ports:
-        if "CH340" in p.description:
-            return p.device
-        
     available_ports = [
         p.device
         for p in serial.tools.list_ports.comports()
-        if 'Arduino' in p.description  # may need tweaking to match new arduinos
+        if 'CH340' in p.description  # may need tweaking to match new arduinos
     ]
     if not available_ports:
         raise IOError("No available port found")
@@ -26,8 +21,6 @@ def get_device_port():
 
 def main():
     port = sys.argv[1] if len(sys.argv) > 1 else get_device_port()
-
-    print(port)
 
     app = QApplication(sys.argv)
 
